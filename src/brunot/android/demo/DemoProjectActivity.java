78ce2c4ade1c086911c1f1dd441872b2265342af
Bruno Torres <bruno.torres@sv.cmu.edu>
2012-04-16 23:12:26 -0700

    Android 2.2.1 Mems intial project

---
+++ src/brunot/android/demo/DemoProjectActivity.java
@@ -0,0 +1,13 @@
+package brunot.android.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DemoProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
