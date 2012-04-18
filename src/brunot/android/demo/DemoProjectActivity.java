73e1b022f67498763c3bd9c359c4413b974b85cb
Bruno Torres <bruno.torres@sv.cmu.edu>
2012-04-17 19:29:51 -0700

    Menu items for MEMS android prototype

--- src/brunot/android/demo/DemoProjectActivity.java
+++ src/brunot/android/demo/DemoProjectActivity.java
@@ -2,12 +2,29 @@ package brunot.android.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DemoProjectActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
+        ListView listView = (ListView) findViewById(R.id.lstOptions);
+        String[] values = new String[] { "Incident", "Reference", "Guidance",
+        	"Route to location", "Alert", "Communicate with other teams"};
+
+        // First paramenter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the View to which the data is written
+        // Forth - the Array of data
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        	android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+        */
         setContentView(R.layout.main);
     }
 }
